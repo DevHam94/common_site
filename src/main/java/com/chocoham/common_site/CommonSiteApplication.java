2a846bf94ba22535b7e00818c728dd96dedb8ca8
admin <gkaeogus7@gmail.com>
2023-10-17 16:03:43 +0900

    Initial commit

---
+++ src/main/java/com/chocoham/common_site/CommonSiteApplication.java
@@ -0,0 +1,13 @@
+package com.chocoham.common_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonSiteApplication.class, args);
+    }
+
+}
